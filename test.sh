0a6e8ac32dbacad07cf542bd4c1347400c64ff8b
Mariette <marietteschonfeld@gmail.com>
2025-01-29 16:18:31 +0100

    shell scripts

---
+++ test.sh
@@ -0,0 +1,24 @@
+datapath=/users/marietteschonfeld/Code/AdversariApple/Data/mvtec_anomaly_detection
+datasets=('bottle')
+dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '"${dataset}"; done))
+
+gpu=0
+
+############# Detection
+### IM224:
+# Baseline: Backbone: WR50, Blocks: 2 & 3, Coreset Percentage: 10%, Embedding Dimensionalities: 1024 > 1024, neighbourhood aggr. size: 3, neighbours: 1, seed: 0
+# Performance: Instance AUROC: 0.992, Pixelwise AUROC: 0.981, PRO: 0.944
+env PYTHONPATH=src python bin/run_patchcore.py --gpu "$gpu" --seed 0 --save_patchcore_model --log_group PINACS_mvtec --log_project MVTecAD_Results results \
+patch_core -b resnet18 -le layer2 -le layer3 --faiss_on_gpu --anomaly_scorer_num_nn 1 --patchsize 3 sampler -p 0.01 approx_greedy_coreset dataset --resize 256 --imagesize 256 "${dataset_flags[@]}" mvtec $datapath
+
+
+datapath=/users/marietteschonfeld/Code/AdversariApple/Data/VisA_20220922
+datasets=('candle')
+dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '"${dataset}"; done))
+
+############# Detection
+### IM224:
+# Baseline: Backbone: WR50, Blocks: 2 & 3, Coreset Percentage: 10%, Embedding Dimensionalities: 1024 > 1024, neighbourhood aggr. size: 3, neighbours: 1, seed: 0
+# Performance: Instance AUROC: 0.992, Pixelwise AUROC: 0.981, PRO: 0.944
+env PYTHONPATH=src python bin/run_patchcore.py --gpu "$gpu" --seed 0 --save_patchcore_model --log_group PINACS_visa --log_project VisA_Results results \
+patch_core -b resnet18 -le layer2 -le layer3 --faiss_on_gpu --anomaly_scorer_num_nn 1 --patchsize 3 sampler -p 0.01 approx_greedy_coreset dataset --resize 256 --imagesize 256 "${dataset_flags[@]}" visa $datapath
