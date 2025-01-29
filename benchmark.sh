78234da41097bc323b6976bede532da38f7b2f49
Mariette <marietteschonfeld@gmail.com>
2025-01-29 16:45:05 +0100

    fais not on gpu

--- benchmark.sh
+++ benchmark.sh
@@ -9,7 +9,7 @@ gpu=3
 # Baseline: Backbone: WR50, Blocks: 2 & 3, Coreset Percentage: 10%, Embedding Dimensionalities: 1024 > 1024, neighbourhood aggr. size: 3, neighbours: 1, seed: 0
 # Performance: Instance AUROC: 0.992, Pixelwise AUROC: 0.981, PRO: 0.944
 env PYTHONPATH=src python bin/run_patchcore.py --gpu "$gpu" --seed 0 --save_patchcore_model --log_group PINACS_mvtec --log_project MVTecAD_Results results \
-patch_core -b resnet18 -le layer2 -le layer3 --faiss_on_gpu --anomaly_scorer_num_nn 1 --patchsize 3 sampler -p 0.01 approx_greedy_coreset dataset --resize 256 --imagesize 256 "${dataset_flags[@]}" mvtec $datapath
+patch_core -b resnet18 -le layer2 -le layer3 --anomaly_scorer_num_nn 1 --patchsize 3 sampler -p 0.01 approx_greedy_coreset dataset --resize 256 --imagesize 256 "${dataset_flags[@]}" mvtec $datapath
 
 
 datapath=/cw/dtaijupiter/NoCsBack/dtai/mariette/AdversariApple/Data/VisA_20220922
@@ -21,4 +21,4 @@ dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '"${dataset}"; don
 # Baseline: Backbone: WR50, Blocks: 2 & 3, Coreset Percentage: 10%, Embedding Dimensionalities: 1024 > 1024, neighbourhood aggr. size: 3, neighbours: 1, seed: 0
 # Performance: Instance AUROC: 0.992, Pixelwise AUROC: 0.981, PRO: 0.944
 env PYTHONPATH=src python bin/run_patchcore.py --gpu "$gpu" --seed 0 --save_patchcore_model --log_group PINACS_visa --log_project VisA_Results results \
-patch_core -b resnet18 -le layer2 -le layer3 --faiss_on_gpu --anomaly_scorer_num_nn 1 --patchsize 3 sampler -p 0.01 approx_greedy_coreset dataset --resize 256 --imagesize 256 "${dataset_flags[@]}" visa $datapath
+patch_core -b resnet18 -le layer2 -le layer3 --anomaly_scorer_num_nn 1 --patchsize 3 sampler -p 0.01 approx_greedy_coreset dataset --resize 256 --imagesize 256 "${dataset_flags[@]}" visa $datapath
